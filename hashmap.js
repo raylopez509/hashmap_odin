649ae07e6b8611eaad7f99b47db0d85281bae7b7
Ray Lopez <raylopez509@gmail.com>
2025-03-07 15:53:52 -0800

    added linkedlist and finished the hashmap added set, get, has, remove, length, clear, keys, values, and entries methods hashmap is done

--- hashmap.js
+++ hashmap.js
@@ -2,8 +2,10 @@
 //   throw new Error("Trying to access index out of bounds");
 // }
 
+import { LinkedList } from "./linkedlist.js";
+
 class Hashmap {
-  loadFactor = 0;
+  loadFactor = 0.75;
   capacity = 16;
   map = new Array(this.capacity);
 
@@ -19,12 +21,145 @@ class Hashmap {
   }
 
   set(key, value) {
-    this.map[this.hash(key)] = [key, value];
+    const index = this.hash(key);
+    if(!this.map.hasOwnProperty(index)) {
+      this.map[index] = new LinkedList();
+    }   
+    let node = this.map[index].head;
+    let foundKey = false;
+    while(node !== null) {
+      if(node.value[0] == key) {
+        node.value = [key,value];
+        foundKey = true;
+      };
+      node = node.nextNode;
+    }
+    if(!foundKey) {
+      this.map[index].append([key,value]);
+    }
+    const loadLevel = this.capacity * this.loadFactor;
+    if(this.length() > loadLevel) {
+      console.log("current load: " + loadLevel);
+      this.capacity = this.capacity * 2;
+      console.log("new load: " + loadLevel * 2);
+      let entries = this.entries();
+      this.map = new Array(this.capacity);
+      for(let entry of entries) {
+        this.set(entry[0],entry[1]);
+      }
+    }
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if(!this.map.hasOwnProperty(index)) {
+      return null;
+    }
+    else {
+      let node = this.map[index].head;
+      while(node !== null) {
+        if(node.value[0] == key) {
+          return node.value[1];
+        };
+        node = node.nextNode;
+      }
+      return null;
+    }
+  }
+
+  has(key) {
+    let value = this.get(key);
+    if(value === null) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    if(!this.map.hasOwnProperty(index)) {
+      return false;
+    }
+    else {
+      let node = this.map[index].head;
+      let foundNodeIndex = 0;
+      while(node !== null) {
+        if(node.value[0] == key) {
+          this.map[index].removeAt(foundNodeIndex);
+          return true;
+        };
+        node = node.nextNode;
+        foundNodeIndex++;
+      }
+      return false;
+    }
+  }
+
+  length() {
+    let length = 0;
+    for(let i = 0; i < this.map.length; i++) {
+      if(this.map.hasOwnProperty(i)) {
+        let node = this.map[i].head;
+        while(node !== null) {
+          length++;
+          node = node.nextNode;
+        }
+      }
+    }
+    return length;
+  }
+
+  clear() {
+    this.map = new Array(this.capacity);
+  }
+
+  keys() {
+    let keys = [];
+    for(let i = 0; i < this.map.length; i++) {
+      if(this.map.hasOwnProperty(i)) {
+        let node = this.map[i].head;
+        while(node !== null) {
+          keys.push(node.value[0]);
+          node = node.nextNode;
+        }
+      }
+    }
+    return keys;
+  }
+
+  values() {
+    let values = [];
+    for(let i = 0; i < this.map.length; i++) {
+      if(this.map.hasOwnProperty(i)) {
+        let node = this.map[i].head;
+        while(node !== null) {
+          values.push(node.value[1]);
+          node = node.nextNode;
+        }
+      }
+    }
+    return values;   
+  }
+
+  entries() {
+    let entries = [];
+    for(let i = 0; i < this.map.length; i++) {
+      if(this.map.hasOwnProperty(i)) {
+        let node = this.map[i].head;
+        while(node !== null) {
+          entries.push(node.value);
+          node = node.nextNode;
+        }
+      }
+    }
+    return entries;       
   }
 }
 
 const test = new Hashmap();
-test.set('apple', 'red')
+test.set('apple', 'red');
 test.set('banana', 'yellow')
 test.set('carrot', 'orange')
 test.set('dog', 'brown')
@@ -36,5 +171,29 @@ test.set('ice cream', 'white')
 test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
-console.log(test.map[1][0]);
+
+console.log("Before")
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+test.set('apple', 'green');
+
+console.log("After")
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+
+test.set('moon','silver');
+console.log("should be scrambled");
+console.log(test.get('moon'));
+console.log(test.has('moon'));
+console.log(test.remove('moon'));
+console.log(test.length());
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+
+test.clear();
+
+console.log(test.entries());
 
---
+++ linkedlist.js
@@ -0,0 +1,171 @@
+export class LinkedList {
+  head = null;
+  tail = null;
+  size = 0;
+
+  append(value) {
+    const newNode = new Node(value);
+    if(this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      let node = this.head;
+      while(node.nextNode != null) {
+        node = node.nextNode;
+      }
+      node.nextNode = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    if(this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      let node = this.head;
+      newNode.nextNode = node;
+      this.head = newNode;
+    }
+    this.size++;
+  }
+
+  at(index) {
+    let node = this.head;
+    for(let i = 0; i < index; i++) {
+      node = this.head.nextNode;
+    } 
+    return node;
+  }
+
+  pop() {
+    let node = this.head;
+    let newTail = this.head;
+    if(this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      
+    } else {
+      node = node.nextNode;
+      while(node.nextNode != null) {
+        newTail = node;
+        node = node.nextNode;
+      }
+      this.tail = newTail;
+      newTail.nextNode = null;
+    }
+    this.size--;
+  }
+
+  contains(value) {
+    let node = this.head;
+    while(node != null) {
+      if(node.value === value) {
+        return true;
+      }
+      node = node.nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    let index = 0;
+    let node = this.head;
+    while(node != null) {
+      if(node.value === value) {
+        break;
+      }
+      node = node.nextNode;
+      index++;
+    }
+    return index;
+  }
+
+  toString() {
+    if(this.head === null) {
+      return "empty list";
+    }
+    else {
+      let str = "";
+      let node = this.head;
+      str += `( ${node.value} )`;
+      while(node.nextNode != null) {
+        node = node.nextNode;
+        str += ` -> ( ${node.value} )`;
+      }
+      str += " -> null";
+      return str;
+    }
+  }
+
+  insertAt(value, index) {
+    let newNode = new Node(value);
+    if(index == 0) {
+      if(this.head == this.tail) {
+        this.tail = newNode.nextNode;
+      }
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+    else {
+      let node = this.head;
+      let prevNode = null;
+      for(let i = 0; i < index; i++) {
+        prevNode = node;
+        node = node.nextNode;
+      }
+      
+      if(prevNode == null) {
+        this.head = newNode;
+        newNode.nextNode = node;
+      }
+      else {
+        prevNode.nextNode = newNode;
+        newNode.nextNode = node;
+      }
+
+      if(newNode.nextNode == null) {
+        this.tail = newNode;
+      }
+    }
+    this.size++;
+  }
+  
+  removeAt(index) {
+    let node = this.head;
+    let prevNode = null;
+    for(let i = 0; i < index; i++) {
+      prevNode = node;
+      node = node.nextNode;
+    }
+    if(prevNode == null) {
+      this.head = node.nextNode;
+    }
+    else {
+      prevNode.nextNode = node.nextNode;
+      if(prevNode.nextNode == null) {
+        this.tail = prevNode;
+      }
+    }
+  }
+}
+
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+// const ll = new LinkedList();
+// ll.append("dog");
+// ll.append("cat");
+// ll.insertAt("mouse", 1);
+// console.log(ll.toString());
+// ll.removeAt(0);
+// console.log(ll.toString());
